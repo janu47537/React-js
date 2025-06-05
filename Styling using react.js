2adedca04006b0156a5c0704d743bdeae51a5ebb
A Janani Priya <janu47537@gmail.com>
2025-06-05 09:59:33 +0530

    Add files via upload

---
+++ Styling using react.js
@@ -0,0 +1,23 @@
+//App.js//
+import './App.css';
+function App(){
+    //Internal Styling//
+  const h={
+    color:'red',
+    backgroundColor:'cyan',
+    border:"5px solid yellow",
+    padding:"20px",
+    margin:"20px",
+    borderRadius:"20px",
+  }
+  return(
+    //Inline Styling
+    <div>
+      <h1 style={h}> Hello MTIET</h1>
+      <h2 style={{color:"green"}}>Hello World!!!!!!</h2>
+    </div>
+
+  );
+    
+}
+export default App;
