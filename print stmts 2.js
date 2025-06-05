6f3b2563c5a7a3cc45060d57fc545a1ec89e2c5b
A Janani Priya <janu47537@gmail.com>
2025-06-05 10:30:22 +0530

    Add files via upload

---
+++ print stmts 2.js
@@ -0,0 +1,18 @@
+//App.js//
+import './App.css';
+function App(){
+  return(
+    <>
+    <h1 className="h">Hello World!!</h1>
+    </>
+  );
+}
+export default App;
+//App.css//
+.h{
+  color:blueviolet;
+  font-size:50px;
+  font-family:Arial, Helvetica, sans-serif;
+  text-decoration: underline;
+  
+}
