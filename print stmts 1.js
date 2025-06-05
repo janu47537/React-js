2974d06dc7f3e9ac93fa19a6ed5ddbe333ffce6d
A Janani Priya <janu47537@gmail.com>
2025-06-05 10:23:09 +0530

    Add files via upload

---
+++ print stmts 1.js
@@ -0,0 +1,14 @@
+//App.js//
+import './App.css';
+function App(){
+  return(
+    <>
+    <h1>Hello</h1>
+    <h2>Hi</h2>
+    <h3>Hi Good Morning</h3>
+    <p>Hello Students</p>
+    <p>Hello World!!!</p>
+    </>
+  );
+}
+export default App;
