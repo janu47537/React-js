8f33a538470add4ddff3ee5168ad98f9eedadc6c
A Janani Priya <janu47537@gmail.com>
2025-06-05 10:07:56 +0530

    Add files via upload

---
+++ External styling.js
@@ -0,0 +1,21 @@
+//App.js//
+import './App.css';
+function App(){
+  return(
+    <div>
+      <center>
+        <h1 class='h'>Hello World!!!</h1>
+        <a href='https://www.googlecom'>Click Me</a>
+      </center>
+    </div>
+  );
+}
+export default App;
+//App.css//
+.h{
+  color:blue;
+  border:5px solid red;
+  background-color:aqua;
+  width:fit-content;
+  border-radius:10px;
+}
