e97fcecb7ebb0645811d91392dfd220625aa49a1
A Janani Priya <janu47537@gmail.com>
2025-06-05 10:15:53 +0530

    Add files via upload

---
+++ Links&Lists.js
@@ -0,0 +1,19 @@
+//App.js//
+ import './App.css';
+function App(){
+  return(
+    <div>
+      <h1>Hello</h1>
+      <a href='https://www.youtube.com'>Click Me</a>
+      <ol start="A" type='A'>
+        <li>Hello</li>
+        <li>Hi</li>
+      </ol>
+      <ul type='square'>
+      <li>Ram</li>
+      <li>Krishna</li>
+      </ul>
+    </div>
+  );
+}
+export default App;
