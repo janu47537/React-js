d9e68204225682499f6adb6e73472b974e7563ac
A Janani Priya <janu47537@gmail.com>
2025-06-05 10:57:40 +0530

    Add files via upload

---
+++ Javascript prog in react.js
@@ -0,0 +1,14 @@
+//App.js//
+import React from 'react';
+function App(){
+  var a=20;
+  var b=30;
+  var c=a+b;
+  c=<p>{c}</p>
+  return(
+    <div>
+      <p>{c}</p>
+    </div>
+  );
+}
+export default App;
