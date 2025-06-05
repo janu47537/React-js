9362020a0ab3ed8c175e46483a3dccd13880f05d
A Janani Priya <janu47537@gmail.com>
2025-06-05 10:46:29 +0530

    Add files via upload

---
+++ Example program.js
@@ -0,0 +1,23 @@
+//example.js/
+import React from 'react';
+function example(){
+    return(
+        <div>
+            <h1>
+                This is Example Page
+            </h1>
+        </div>
+    );
+}
+export default example;
+//Appjs//
+import React from 'react';
+import Example from './example';
+function App(){
+  return(
+    <div>
+      <Example />
+    </div>
+  );
+}
+export default App;
